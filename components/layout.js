a6c92a3cc2521579d5fe5849c2825b6375666219
Tanasak Pusawatwong <tanasak32123@gmail.com>
2023-01-22 01:58:54 +0700

    Add my profile

--- components/layout.js
+++ components/layout.js
@@ -1,15 +1,15 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from './layout.module.css';
-import utilStyles from '../styles/utils.module.css';
-import Link from 'next/link';
+import Head from "next/head";
+import Image from "next/image";
+import styles from "./layout.module.css";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
 
-const name = 'Tanasak Pusawatwong';
-export const siteTitle = 'Next.js Sample Website';
+const name = "Tanasak Pusawatwong";
+export const siteTitle = "Next.js Sample Website";
 
-export default function Layout({children, home}) {
-    return (
-        <div className={styles.container}>
+export default function Layout({ children, home }) {
+  return (
+    <div className={styles.container}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <meta
@@ -19,7 +19,7 @@ export default function Layout({children, home}) {
         <meta
           property="og:image"
           content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle,
+            siteTitle
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
         <meta name="og:title" content={siteTitle} />
@@ -30,7 +30,7 @@ export default function Layout({children, home}) {
           <>
             <Image
               priority
-              src="/images/profile.jpg"
+              src="/images/profile.jpeg"
               className={utilStyles.borderCircle}
               height={144}
               width={144}
@@ -65,5 +65,5 @@ export default function Layout({children, home}) {
         </div>
       )}
     </div>
-    );
+  );
 }
